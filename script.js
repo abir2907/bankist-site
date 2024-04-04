c95deb8614e49286b3532fb2dd82be4f0344399c
Abir Chodha <abirchodha1@gmail.com>
2024-04-05 02:39:03 +0530

    Implement smooth scrolling using event delegation

--- script.js
+++ script.js
@@ -47,10 +47,12 @@ btnScrollTo.addEventListener('click', function (e) {
 ///////////////////////////////////////
 // Page Navigation
 
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  });
+  }
 });
