a03dd0c0f11554273c392955d66aa07e5b8ec28e
Abir Chodha <abirchodha1@gmail.com>
2024-04-04 21:13:31 +0530

    Make scrolling smooth

--- script.js
+++ script.js
@@ -39,8 +39,9 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
 });
